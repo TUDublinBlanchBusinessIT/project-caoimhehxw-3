b22dd3594155a49360f2b761a66320cb9cbd8262
caoimhe <B00153330@mytudublin.ie>
2025-04-26 16:25:24 +0100

    CourseController set up and basic course page

---
+++ resources/views/courses/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+<div class="container mt-5">
+    <h2>All Courses</h2>
+
+    <p>Courses page is working!</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\CourseController;
+
+Route::resource('courses', CourseController::class);
+
 
 Route::get('/', function () {
     return redirect('/students');
 });
 
 Route::resource('students', StudentController::class);
+Route::resource('courses', CourseController::class);
